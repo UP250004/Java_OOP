91840451ee90c00d06d487165fb5c6ff1e3140fb
UP250004 <up250004@alumnos.upa.edu.mx>
2025-09-30 13:55:02 -0600

    .

---
+++ E16Metodos.java
@@ -0,0 +1,28 @@
+class E16Metodos{
+    public static void main(String[] args){
+        String palabra = "holaMundo";
+        escribir(palabra);
+        Marcador marcadorBlanco = new Marcador("Blanco", i);
+    }
+    static void escribir(String texto){
+        for(char letra: texto.toCharArray()){
+            if(this.nivelTinta > 0){
+            sop(letra)
+            this.nivelTinta--;//this.nivelTinta = this.nivelTinta - 1;
+            }
+        }
+
+        }
+    }
+
+}
+
+class Marcador{
+    String colorMarcador;
+    short nivelTinta;
+
+    Marcador(String color, short porcentaje){
+        this.nivelTinta = 3;
+        this.colorMarcador = color;
+    }
+}
