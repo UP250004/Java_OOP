67f5aaef086cc59890e627c4da665a31dd3fa243
UP250004 <up250004@alumnos.upa.edu.mx>
2025-10-08 19:03:43 -0600

    .

--- P2E1ExMarker.java
+++ P2E1ExMarker.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 
-public class P2E1ExMarker{
+public class E16Metodos {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner palabraUser = new Scanner(System.in);
         
         // Se crean los marcadores de diferentes colores
         Marcador marcadorRojo = new Marcador("Rojo");
-        Marcador marcadorBlanco = new Marcador(); // Usará el color predeterminado (blanco)
+        Marcador marcadorBlanco = new Marcador(); // Constructor por defecto
         Marcador marcadorVerde = new Marcador("Verde");
         Marcador marcadorAzul = new Marcador("Azul");
-        
-        System.out.println("Por favor, ingresa la palabra que deseas escribir:");
-        String palabra = scanner.nextLine();
-        
-        // Escribimos con cada marcador
+        /*
         System.out.println("\u001b[31m" + "\n--- Escribiendo con el marcador Rojo ---");
         escribir(palabra, marcadorRojo, "\u001b[31m");
         System.out.println("\u001b[32m" + "\n--- Escribiendo con el marcador Verde ---");
@@ -22,24 +18,27 @@ public class P2E1ExMarker{
         escribir(palabra, marcadorAzul, "\u001b[34m");
         System.out.println("\u001b[0m" + "\n--- Escribiendo con el marcador Blanco ---");
         escribir(palabra, marcadorBlanco, "\u001b[0m");
+        */
+        System.out.println("Por favor, ingresa la palabra que deseas escribir:");
+        String palabra = palabraUser.nextLine();
         
+        // El objeto marcador ahora se encarga de escribir por sí mismo
+        System.out.println("\u001b[0m" + "\n--- Escribiendo con el marcador Rojo ---");
+        marcadorRojo.escribir(palabra,"\u001b[31m");
+        
+        System.out.println("\u001b[0m" + "\n--- Escribiendo con el marcador Blanco ---");
+        marcadorBlanco.escribir(palabra,"\u001b[0m");
+        
+        System.out.println("\u001b[0m" + "\n--- Escribiendo con el marcador Verde ---");
+        marcadorVerde.escribir(palabra,"\u001b[32m");
+        
+        System.out.println("\u001b[0m" + "\n--- Escribiendo con el marcador Azul ---");
+        marcadorAzul.escribir(palabra,"\u001b[34m");
         
         scanner.close();
     }
-
-    public static void escribir(String texto, Marcador marcador, String codigo) {
-        System.out.println("Escribiendo con un marcador de color: " + marcador.getColorMarcador() + codigo);
-        for (char letra : texto.toCharArray()) {
-            if (marcador.getNivelTinta() > 0) {
-                System.out.println(letra + codigo);
-                marcador.decrementarNivelTinta();
-            } else {
-                System.out.println("¡El marcador " + marcador.getColorMarcador() + " se ha quedado sin tinta!");
-                break;
-            }
-        }
-    }
 }
+
 class Marcador {
     String colorMarcador;
     short nivelTinta;
@@ -50,10 +49,9 @@ class Marcador {
         this.colorMarcador = color;
     }
 
-    // Constructor sin parámetros (constructor por defecto)
-    // Asigna el color "Blanco" por defecto
+    // Constructor por defecto
     Marcador() {
-        this("Blanco"); // Llama al otro constructor con "Blanco"
+        this("Blanco");
     }
     
     public short getNivelTinta() {
@@ -67,6 +65,19 @@ class Marcador {
     public void decrementarNivelTinta() {
         if (this.nivelTinta > 0) {
             this.nivelTinta--;
+        }
+    }
+
+    public void escribir(String texto,String color) {
+        System.out.println("Escribiendo con un marcador de color: " + this.getColorMarcador() + "color");
+        for (char letra : texto.toCharArray()) {
+            if (this.getNivelTinta() > 0) {
+                System.out.println(color + letra);
+                this.decrementarNivelTinta();
+            } else {
+                System.out.println("¡El marcador " + this.getColorMarcador() + " se ha quedado sin tinta!");
+                break;
+            }
         }
     }
 }
