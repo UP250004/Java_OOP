2fad43bddf55c41fb393582467b713e99135c3f0
UP250004 <up250004@alumnos.upa.edu.mx>
2025-11-06 12:02:27 -0600

    .

--- E29Mueble/App.java
+++ E29Mueble/App.java
@@ -1,9 +1,10 @@
 
 public class App {
     public static void main(String ... args){
+        boolean vendido = false;
         Silla silla = new Silla(2,2,4,87.8f,95,"Piel carnero");
-        silla.venderse(100f);
-
+        vendido = silla.venderse(100f);
+        System.out.println(vendido);
         silla.sentarse();
     }
 
